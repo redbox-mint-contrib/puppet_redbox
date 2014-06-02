856c369bfa8781040c49492fe84ae441efb7f143
Shilo Banihit <shilo@redboxresearchdata.com.au>
2014-06-02 10:37:43 +1000

    REDBOX-244 #comment Installing in correct order of execs, including groovy and sample script.

--- manifests/add_oaipmh_stack.pp
+++ manifests/add_oaipmh_stack.pp
@@ -28,19 +28,23 @@ define puppet-redbox::add_oaipmh_stack (
   $oaiharvester_package_url="http://dev.redboxresearchdata.com.au/nexus/service/local/artifact/maven/redirect?r=snapshots&g=au.com.redboxresearchdata.oai&a=redbox-oai-feed&v=LATEST&e=zip&c=bin",
   $hm_url="http://localhost:8080/json-harvester-manager/harvester/",
   $oaiharvester_id="redbox-oai-pmh-feed",
+  $oaiharvester_samplehelper = "https://raw.githubusercontent.com/redbox-harvester/redbox-oai-feed/master/support/install/addSampleRecord.groovy",
   $oaiserver_formats_url = "http://localhost/oai-server/?verb=ListMetadataFormats",
+  $groovy_version="2.2.2",
+  $groovy_install_url="http://dl.bintray.com/groovy/maven/groovy-binary-",
+  $groovy_install_dir="/opt/groovy",
   ) {
   ## Install Postgres
   class { 'postgresql::globals':
 	  manage_package_repo => true,
 	  version             => $pgsql_version,
-	  pg_hba_conf_defaults=>false
-	}->class { 'postgresql::server':}
-  
+	  pg_hba_conf_defaults=>false,
+	}
+	
 	postgresql::server::db { 'oaiserver':
 	  user     => 'oaiserver',
 	  password => postgresql_password('oaiserver', 'oaiserver'),
-	}
+	} 
 	postgresql::server::db { 'curationmanager':
     user     => 'curationmanager',
     password => postgresql_password('curationmanager', 'curationmanager'),
@@ -49,14 +53,14 @@ define puppet-redbox::add_oaipmh_stack (
     type => 'local',
     database => 'all',
     user => 'postgres',
-    auth_method => 'ident'
+    auth_method => 'ident',
   } 
   postgresql::server::pg_hba_rule { 'allow localhost TCP access to postgresql user':
     type => 'host',
     database => 'all',
     user => 'postgres',
     address => '127.0.0.1/32',
-    auth_method => 'md5'
+    auth_method => 'md5',
   } 
 	postgresql::server::pg_hba_rule { 'allow trust access from localhost':
 	  description => "allow trust access from localhost",
@@ -64,20 +68,15 @@ define puppet-redbox::add_oaipmh_stack (
 	  database => 'all',
 	  user => 'all',
 	  address => '127.0.0.1/32',
-	  auth_method => 'trust'
+	  auth_method => 'trust',
 	} 
 	postgresql::server::pg_hba_rule { 'allow trust access from local':
     description => "allow trust access from localhost",
     type => 'local',
     database => 'all',
     user => 'all',
-    auth_method => 'trust'
-  }
-  exec {"Download and apply SQL init file":
-    cwd     => '/tmp',
-    command => "${wget_cmd} ${$oaiserver_config_src}${oaiserver_init_sql} && psql -U oaiserver < ${oaiserver_init_sql}",
-    path    => ['/usr/bin','/usr/sbin', '/bin']
-  }
+    auth_method => 'trust',
+  } 
   
   # Install Tomcat and install app WARs and their configuration
   service {"Stop any existing Tomcat":
@@ -85,6 +84,7 @@ define puppet-redbox::add_oaipmh_stack (
     hasstatus => false,
     status    => "cat /var/run/tomcat/tomcat7.pid",
     stop      => "kill `cat /var/run/tomcat/tomcat7.pid` && rm -rf /var/run/tomcat/tomcat7.pid",
+    require  => Class['postgresql::server'],
   } -> user {"Add tomcat user":
     name    => "tomcat",
     ensure  => present,
@@ -94,13 +94,15 @@ define puppet-redbox::add_oaipmh_stack (
   } -> exec { "Download Tomcat Bundle":
     cwd     => '/tmp/',
     creates => "/tmp/${tomcat_package}",
-    command => "rm -rf ${tomcat_install} && ${wget_cmd} ${tomcat_package_url}${tomcat_package}",
-    path    => ['/usr/bin','/usr/sbin', '/bin', '/sbin']
+    command => "${wget_cmd} ${tomcat_package_url}${tomcat_package}",
+    path    => ['/usr/bin','/usr/sbin', '/bin', '/sbin'],
+    unless    => "ls -l ${tomcat_install}",
   } -> exec {"Extract Tomcat Bundle to Tomcat install directory":
     cwd       => '/tmp/', 
     creates   => '/tmp/${tomcat_package_dir}',
     command   => "tar xzf ${tomcat_package} && mv -f ${tomcat_package_dir} ${tomcat_install} && rm -rf ${tomcat_package} && curl -o ${tomcat_install}/bin/setenv.sh ${oaiserver_config_src}setenv.sh && chmod +x ${tomcat_install}/bin/setenv.sh && chown -R tomcat:tomcat ${tomcat_install} && curl -o /etc/init.d/tomcat7 ${oaiserver_config_src}tomcat7 && chmod +x /etc/init.d/tomcat7",
-    path      => ['/usr/bin','/usr/sbin', '/bin', '/sbin']
+    path      => ['/usr/bin','/usr/sbin', '/bin', '/sbin'],
+    unless    => "ls -l ${tomcat_install}",
   } -> file {"${harvester_install_dir}":
       ensure => directory,
       owner => "tomcat",
@@ -122,11 +124,15 @@ define puppet-redbox::add_oaipmh_stack (
   } -> exec {"Download oai-server, json-harvester-manager and curation manager WAR files and other config files": 
     cwd     => "${tomcat_install}/webapps/",
     command => "${wget_download_cmd} json-harvester-manager.war '${hm_war_url}' && ${wget_download_cmd} CurationManager.war '${cm_war_url}' && ${wget_download_cmd} oai-server.war '${oaiserver_war_url}'",
-    path    => ['/usr/bin','/usr/sbin', '/bin', '/sbin']
+    path    => ['/usr/bin','/usr/sbin', '/bin', '/sbin'],
   } -> exec {"Download the config files":
     cwd     => "${cm_workdir}",
     command => "${wget_cmd} '${cm_spring_idp_config_src}${cm_idp_1}' && ${wget_cmd} '${cm_spring_idp_config_src}${cm_idp_2}' && ${wget_cmd} '${cm_spring_idp_config_src}${cm_idp_3}' && ${wget_cmd} '${cm_spring_idp_config_src}${cm_idp_4}' && ${wget_cmd} '${cm_spring_idp_config_src}${cm_idp_5}' && ${wget_cmd} '${$cm_spring_config_src}${cm_spring_config_resource}' && ${wget_cmd} '${cm_handler_key_url}'",
     path    => ['/usr/bin','/usr/sbin', '/bin', '/sbin'],
+  } -> exec {"Download and apply SQL init file":
+    cwd     => '/tmp',
+    command => "${wget_cmd} ${oaiserver_config_src}${oaiserver_init_sql} && psql -U oaiserver < ${oaiserver_init_sql}",
+    path    => ['/usr/bin','/usr/sbin', '/bin'],
   } -> service {"Configure Tomcat service":
     name => "tomcat7",
     ensure => running,
@@ -136,11 +142,26 @@ define puppet-redbox::add_oaipmh_stack (
     cwd     => "/tmp",
     command => "curl -L -o ${oaiharvester_package} '${oaiharvester_package_url}' && curl -i -F 'harvesterPackage=@${oaiharvester_package}' -H 'Accept: application/json' '${hm_url}upload/${oaiharvester_id}' && curl -o harvester.check -i -H 'Accept: application/json' '${hm_url}' &&  grep '${oaiharvester_id}' harvester.check >/dev/null",
     path    => ['/usr/bin','/usr/sbin', '/bin', '/sbin']
-  } -> exec {"Staring OAI-PMH FEED harvester":
+  } -> exec {"Starting OAI-PMH FEED harvester":
     cwd     => "/tmp",
-    command => "curl -o harvester.check -i -H 'Accept: application/json' '${hm_url}start/${oaiharvester_id}' &&  grep true harvester.check >/dev/null",
+    command => "curl -o harvester.check -i -H 'Accept: application/json' '${hm_url}start/${oaiharvester_id}'",
     path    => ['/usr/bin','/usr/sbin', '/bin', '/sbin']
-  } 
+  } -> package {"Install unzip":
+      name  => "unzip",
+  } -> exec {"Downloading groovy...":
+    cwd     => "/tmp",
+    command => "${wget_download_cmd} groovy-binary-${groovy_version}.zip ${groovy_install_url}${groovy_version}.zip && unzip groovy-binary-${groovy_version}.zip && mv /tmp/groovy-${groovy_version} ${groovy_install_dir} && ln -s ${groovy_install_dir}/bin/groovy /usr/bin/groovy",
+    unless  => "ls -l ${groovy_install_dir}",
+    path    => ['/usr/bin','/usr/sbin', '/bin'],
+  } -> exec {"Downloading helper script for adding sample data.":
+    cwd     => "/opt/harvester",
+    command => "${wget_download_cmd} addSampleRecord.groovy ${oaiharvester_samplehelper} && chmod +x addSampleRecord.groovy",
+    path    => ['/usr/bin','/usr/sbin', '/bin'],
+  } -> exec {"Priming OAI-Server": 
+    cwd     => "/opt/harvester",
+    command => "curl --retry 2 --retry-delay 60 ${$oaiserver_formats_url}",
+    path    => ['/usr/bin','/usr/sbin', '/bin'],
+  }
   
-  
+  include postgresql::server
 }
