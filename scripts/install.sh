9b14e7d2d3ad7430e78f19bc47cb265f35651ede
mattRedBox <matt@redboxresearchdata.com.au>
2016-01-20 20:03:36 +1000

    RDSGEN22: added download tool to install script.

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,6 +16,9 @@ mkdir -p /var/log/puppet
 export RUBY_VERSION=2.0.0-p598
 export PUPPET_VERSION=3.8.4
 
+# grab tool to download scripts
+yum -y install wget
+
 export PUPPET_INSTALL_DIR=/tmp/ruby_puppet
 mkdir -p ${PUPPET_INSTALL_DIR}
 wget -N -O ${PUPPET_INSTALL_DIR}/install.sh https://raw.githubusercontent.com/redbox-mint-contrib/ruby_puppet/master/Centos/install.sh 
