4a0692d7fb790c7d23210ea6e09c7f7c5d408f72
mattRedBox <matt@redboxresearchdata.com.au>
2016-01-20 16:20:20 +1000

    try basic test with beaker.

---
+++ spec/acceptance/puppet_redbox_install_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper_acceptance'
+
+describe 'puppet_redbox pre-install environment' do
+  #  it 'should install redbox with no errors' do
+  #    shell_result = shell("cd /usr/share/puppet/modules/puppet_redbox/scripts && install.sh")
+  #    expect(shell_result.exit_code).to eq 0
+  #    expect(shell_result.stdout).not_to match /Fail/
+  #    expect(shell_result.stdout).not_to match /err/
+  #  end
+  it 'should output correct platform details' do
+    shell_result = shell("cat /etc/redhat-release")
+    expect(shell_result.exit_code).to eq 0
+    expect(shell_result.stdout).to match /CentOS.*release 6.7/
+  end
+  it 'should show puppet_redbox install script' do
+    shell_result = shell("ls -l /tmp/install.sh")
+    expect(shell_result.exit_code).to eq 0
+    expect(shell_result.stdout).to match /install.sh/
+  end
+end
+
+describe 'puppet_redbox basic install' do
+#  it 'should install ruby, puppet and puppet_rebox without errors' do
+#    shell_result = shell("/tmp/install.sh")
+#    expect(shell_result.exit_code).to eq 0
+#  end
+#  it 'should show default environment is production' do
+#    shell_result = shell("puppet config print environment")
+#    expect(shell_result.exit_code).to eq 0
+#    expect(shell_result.stdout).to match /production/
+#  end
+end
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,29 +1,31 @@
 ## TODO : Once deployment using Docker, uncomment, integrate and build acceptance tests with beaker using docker hypervisor.
 ## until in place, just use rspec tests.
 
-## consul/spec/spec_helper_acceptance.rb
-#require 'beaker-rspec'
-#
-## Not needed for this example as our docker files have puppet installed already
-##hosts.each do |host|
-##  # Install Puppet #  install_puppet
-##end
-#
-#RSpec.configure do |c|
-#  # Project root
-#  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-#
-#  # Readable test descriptions
-#  c.formatter = :documentation
-#
-#  # Configure all nodes in nodeset
-#  c.before :suite do
-#    # Install module and dependencies
-#    puppet_module_install(:source => proj_root, :module_name => 'puppet_redbox')
-#    hosts.each do |host|
-#      # Needed for the consul module to download the binary per the modulefile
-#      on host, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
-#      on host, puppet('module', 'install', 'puppetlabs/vcsrepo'), { :acceptable_exit_codes => [0,1] }
-#    end
-#  end
-#end
+# consul/spec/spec_helper_acceptance.rb
+require 'beaker-rspec'
+
+# Not needed as install script takes care of this
+redbox_script = ['scripts/install.sh']
+hosts.each do |host|
+  scp_to host, File.expand_path(File.join(File.dirname(__FILE__), '..', redbox_script)), "/tmp/install.sh"
+  on host, "chmod +x /tmp/install.sh"
+end
+
+RSpec.configure do |c|
+  # Project root
+  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+  # Readable test descriptions
+  c.formatter = :documentation
+
+  # Configure all nodes in nodeset
+  c.before :suite do
+    # Install module and dependencies
+    #    puppet_module_install(:source => proj_root, :module_name => 'puppet_redbox', :target_module_path => '/usr/share/puppet/modules')
+    #    hosts.each do |host|
+    #      # Needed for the consul module to download the binary per the modulefile
+    #      on host, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
+    #      on host, puppet('module', 'install', 'puppetlabs/vcsrepo'), { :acceptable_exit_codes => [0,1] }
+    #    end
+  end
+end
