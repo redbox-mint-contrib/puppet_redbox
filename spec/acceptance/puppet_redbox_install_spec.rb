615f01e0bb73ed1d747aeb6af62a5075f22821c5
mattRedBox <matt@redboxresearchdata.com.au>
2016-01-21 10:33:27 +1000

    RDSGEN-22. added check for gem.

--- spec/acceptance/puppet_redbox_install_spec.rb
+++ spec/acceptance/puppet_redbox_install_spec.rb
@@ -29,6 +29,11 @@ describe 'puppet_redbox basic install' do
     expect(shell_result.exit_code).to eq 0
     expect(shell_result.stdout).to match /ruby/
   end
+  it 'should show gem version' do
+      shell_result = shell("gem --version")
+      expect(shell_result.exit_code).to eq 0
+      expect(shell_result.stdout).to match /gem/
+    end
   #  it 'should show default environment is production' do
   #    shell_result = shell("puppet config print environment")
   #    expect(shell_result.exit_code).to eq 0
