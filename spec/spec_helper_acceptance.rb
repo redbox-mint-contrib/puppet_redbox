eabd7d9509bb407f601c66b745bb9809b897ffe3
mattRedBox <matt@redboxresearchdata.com.au>
2016-02-02 09:40:32 +1000

    experiment with use of other environment variables for build.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -4,7 +4,7 @@ redbox_script = ['scripts/install.sh']
 hosts.each do |host|
   scp_to host, File.expand_path(File.join(File.dirname(__FILE__), '..', redbox_script)), "/tmp/install.sh"
   on host, "chmod +x /tmp/install.sh"
-  on host, "bash -l -c /tmp/install.sh"
+  on host, "bash -l -c /tmp/install.sh $HARVESTER_ARGS"
 end
 
 RSpec.configure do |c|
