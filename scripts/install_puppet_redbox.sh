5f14e2d82cb57ac48b8de95ce7bdaa34ec656cef
mattRedBox <matt@redboxresearchdata.com.au>
2016-02-02 18:14:07 +1000

    RDSGEN-22. updated concat to match requirements for QCIF sysadmins.

--- scripts/install_puppet_redbox.sh
+++ scripts/install_puppet_redbox.sh
@@ -22,7 +22,7 @@ mkdir -p /var/log/puppet
 touch $LOG_DEST
 
 # Install Basic Puppet Modules
-puppet module install --force --version 1.0.2 puppetlabs/concat
+puppet module install --force --version 1.2.1 puppetlabs/concat
 puppet module install --force --version 4.3.2 puppetlabs/stdlib
 puppet module install --force --version 1.1.1 puppetlabs/apache
 puppet module install --force --version 1.3.2 puppetlabs/vcsrepo
