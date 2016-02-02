28d94c4d64a7a070dba639337b0145ba24be4806
mattRedBox <matt@redboxresearchdata.com.au>
2016-02-02 18:08:13 +1000

    refactored env syntax for beaker dsl.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -4,7 +4,11 @@ redbox_script = ['scripts/install.sh']
 hosts.each do |host|
   scp_to host, File.expand_path(File.join(File.dirname(__FILE__), '..', redbox_script)), "/tmp/install.sh"
   on host, "chmod +x /tmp/install.sh"
-  on host, "bash -l -c /tmp/install.sh ENV['HARVESTER_ARGS']"
+  if ENV['PUPPET_REDBOX_SCRIPT_ARGS']
+    on host, "bash -l -c /tmp/install.sh " +  ENV['PUPPET_REDBOX_SCRIPT_ARGS']
+  else
+    on host, "bash -l -c /tmp/install.sh"
+  end
 end
 
 RSpec.configure do |c|
@@ -13,5 +17,8 @@ RSpec.configure do |c|
 
   # Configure all nodes in nodeset
   c.before :suite do
+    hosts.each do |host|
+      on host, "echo $LOGNAME"
+    end
   end
 end
