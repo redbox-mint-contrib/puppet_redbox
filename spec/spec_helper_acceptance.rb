7bad7416f453c57081c06c3a23edba65e1a9a710
mattRedBox <matt@redboxresearchdata.com.au>
2016-02-18 18:27:02 +1000

    RDSGEN-22: added config to enable working centos7. added master-agent test integration.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,10 +1,86 @@
 require 'beaker-rspec'
 
 redbox_script = ['scripts/install.sh']
-hosts.each do |host|
-  scp_to host, File.expand_path(File.join(File.dirname(__FILE__), '..', redbox_script)), "/tmp/install.sh"
-  on host, "chmod +x /tmp/install.sh"
-  on host, "bash -l -c /tmp/install.sh"
+puppet_conf = ['spec/resources/puppet.conf']
+master_fqdn = ""
+ruby_version = "2.0.0-p598"
+puppet_version = "3.8.4"
+if any_hosts_as?('master')
+  on master, "rpm -Uvh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm"
+  on master, "yum install -y puppet"
+  on master, "yum -y update"
+  on master, "yum update -y ruby"
+  on master, "yum update -y puppet"
+  on master, "facter fqdn" do
+    master_fqdn = stdout
+  end
+  on master, "touch /etc/puppet/autosign.conf"
+  on master, "echo '*' > /etc/puppet/autosign.conf"
+  on master, "hostname #{master_fqdn}"
+  config = {
+    'main' => {
+    'logdir'   => '/var/log/puppet',
+    'vardir'   => '/var/lib/puppet',
+    'ssldir'   => '/var/lib/puppet/ssl',
+    'rundir'   => '/var/run/puppet'
+    },
+    'agent' => {
+    'classfile' => '/var/lib/puppet/classes.txt',
+    'localconfig' => '/var/lib/puppet/localconfig'
+    }
+  }
+  configure_puppet_on(master, config)
+  #    on master, "yum -y install msgpack"
+  ## facter fqdn is reliable way to ensure master-agent exchange matches on certificate
+  #    on master, 'echo "server=$(facter fqdn)" >> /etc/puppet/puppet.conf'
+  on master, "cat /etc/hosts"
+  on master, "cat /etc/puppet/puppet.conf"
+  on master, "ruby --version"
+  on master, "puppet --version"
+  deploy_dir = "/tmp"
+  on master, "mkdir -p #{deploy_dir}"
+  copy_module_to master, :target_module_path => deploy_dir
+  ## clone any git module dependencies
+  git_dependencies = ["puppet_common"]
+  on master, "yum install -y git"
+  git_dependencies.each do |d|
+    clone_git_repo_on master, "/tmp", {:name => "puppet_common", :path => "https://github.com/redbox-mint-contrib/puppet_common.git", :rev => "master"}
+  end
+  ## install modules, including dependencies
+  on master, "yum install -y tar"
+  (["puppet_redbox"].concat(git_dependencies)).each do |puppet_module|
+    on master, "tar -Pcvz #{deploy_dir}/#{puppet_module} -f #{deploy_dir}/#{puppet_module}.tar.gz --exclude=#{deploy_dir}/#{puppet_module}/.git"
+    on master, "puppet module install #{deploy_dir}/#{puppet_module}.tar.gz"
+  end
+  ## add site manifest
+  on master, "mkdir -p /etc/puppet/manifests"
+  on master, "touch /etc/puppet/manifests/site.pp"
+  on master, 'echo "node default { include \"puppet_redbox\" }" > /etc/puppet/manifests/site.pp'
+  on master, "cat /etc/puppet/manifests/site.pp"
+  ## start master and agent
+  #    on master, "mkdir -p /etc/puppet/modules/concat/facts.d && mkdir -p /etc/puppet/modules/conct/facts.d"
+  #    on master, "export FACTER_concat_basedir=/tmp"
+  #    on master, "puppet apply -e \"class {'puppet_common::update_fqdn': updated_fqdn => '#{master_fqdn}'}\""
+  on master, "puppet master -d"
+  on master, "ps -efl | grep puppet"
+  on agents, "yum install -y git"
+  clone_git_repo_on agents, "/tmp", {:name => "ruby_puppet", :path => "https://github.com/redbox-mint-contrib/ruby_puppet.git", :rev => "master"}
+  on agents, "chmod +x /tmp/ruby_puppet/Centos/install.sh"
+  on agents, "export RUBY_VERSION=#{ruby_version};export PUPPET_VERSION=#{puppet_version};/tmp/ruby_puppet/Centos/install.sh"
+  on agents, "source /root/.bashrc && ruby --version && puppet --version"
+  on agents, "mkdir -p /etc/puppet"
+  on agents, "touch /etc/puppet/puppet.conf"
+  configure_puppet_on(agent_only, config)
+  on agents, "echo \"server=#{master_fqdn}\" >> /etc/puppet/puppet.conf"
+  on agents, "cat /etc/hosts"
+  on agents, "cat /etc/puppet/puppet.conf"
+  on agents, "sleep 5 && puppet agent --test --waitforcert 30"
+else
+  hosts.each do |host|
+    scp_to host, File.expand_path(File.join(File.dirname(__FILE__), '..', redbox_script)), "/tmp/install.sh"
+    on host, "chmod +x /tmp/install.sh"
+    on host, "bash -l -c /tmp/install.sh"
+  end
 end
 
 RSpec.configure do |c|
