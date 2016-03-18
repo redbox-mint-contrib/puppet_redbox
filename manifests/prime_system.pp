7d6c6e2a69d652efa7814eb846b19659a55648ae
mattRedBox <matt@redboxresearchdata.com.au>
2016-03-18 14:27:38 +1000

    corrected quotes use.

--- manifests/prime_system.pp
+++ manifests/prime_system.pp
@@ -1,6 +1,6 @@
 define puppet_redbox::prime_system ($system = $title,) {
   exec { "${system}-primer":
-    command     => "wget --no-check-certificate --tries=2 --wait=5 --spider -O /dev/null ${system} || echo 'attempted to prime ${system}",
+    command     => "wget --no-check-certificate --tries=2 --wait=5 --spider -O /dev/null ${system} || echo \"attempted to prime ${system}\"",
     tries       => 2,
     try_sleep   => 3,
     refreshonly => true,
