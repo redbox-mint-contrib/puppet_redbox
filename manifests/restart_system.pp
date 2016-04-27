9982980ac7af1fb1373e8a4a88072b35bd4c50ec
mattRedBox <matt@redboxresearchdata.com.au>
2016-04-28 09:28:19 +1000

    ensure restart already has redbox package in place.

--- manifests/restart_system.pp
+++ manifests/restart_system.pp
@@ -1,5 +1,9 @@
 define puppet_redbox::restart_system ($packages = $title,) {
   $redbox_system = $packages[system]
+  $redbox_package = $packages[package]
 
-  exec { "restart after overlay for ${redbox_system}": command => "service ${redbox_system} restart" }
+  exec { "restart after overlay for ${redbox_system}":
+    command => "service ${redbox_system} restart",
+    require => Package[$redbox_package],
+  }
 }
