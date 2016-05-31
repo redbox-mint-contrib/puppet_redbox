3566b52ca3a5a3badd09e25a56fd9141b58f139d
mattRedBox <matt@redboxresearchdata.com.au>
2016-05-31 13:44:45 +1000

    first step to removing server url update and keeping in institutional overlays.

--- manifests/update_server_env.pp
+++ manifests/update_server_env.pp
@@ -1,18 +1,23 @@
 define puppet_redbox::update_server_env (
-  $server_path = $title,
-  $tf_env      = undef,
-  $has_ssl     = false,
-  $server_url  = undef,) {
+  $server_path               = $title,
+  $tf_env                    = undef,
+  $has_ssl                   = false,
+  $server_url                = undef,
+  $has_institutional_overlay = true) {
   if ($has_ssl) {
     $protocol = https
   } else {
     $protocol = http
   }
 
-  file_line { "update_server_url_${server_path}":
-    path  => $server_path,
-    line  => "export SERVER_URL=\"${protocol}://${server_url}/\"",
-    match => "^export SERVER_URL=\".*\"$",
+  if ($has_institutional_overlay) {
+    notify { "There is an institutional overlay present, so server url should only be updated there.": }
+  } else {
+    file_line { "update_server_url_${server_path}":
+      path  => $server_path,
+      line  => "export SERVER_URL=\"${protocol}://${server_url}/\"",
+      match => "^export SERVER_URL=\".*\"$",
+    }
   }
 
   if ($tf_env and $tf_env[server_environment]) {
