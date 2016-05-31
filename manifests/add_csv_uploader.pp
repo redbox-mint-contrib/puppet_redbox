88c1701e552a2054125b8c67f8c509c85ad2f833
mattRedBox <matt@redboxresearchdata.com.au>
2016-05-31 11:17:15 +1000

    problems with dependencies of nanliu/archive, so moving to simple implementation with unzip only.

--- manifests/add_csv_uploader.pp
+++ manifests/add_csv_uploader.pp
@@ -1,30 +1,38 @@
 define puppet_redbox::add_csv_uploader (
-  $source        = $title,
-  $extract_path  = '/opt',
-  $download_path = '/tmp',
-  $can_download_and_overwrite        = true,
-  $expected_extracted_directory_name = undef,) {
-  require 'archive'
-  $creates = $can_download_and_overwrite ? {
-    true    => undef,
-    default => "${extract_path}/${expected_extracted_directory_name}",
+  $source                     = $title,
+  $extract_path               = '/opt',
+  $download_path              = undef,
+  $can_download_and_overwrite = true,) {
+#  require 'archive'
+
+  if (!$download_path) {
+    fail("Must define an absolute download path including filename.")
   }
+
+  #  $creates = $can_download_and_overwrite ? {
+  #    true    => undef,
+  #    default => "${extract_path}/${expected_extracted_directory_name}",
+  #  }
+
 
   validate_absolute_path($extract_path)
   validate_absolute_path($download_path)
 
-  $archive_name = basename($source)
-  $full_download_path = "${download_path}/${archive_name}"
+  #  $archive_name = basename($source)
+  #  $full_download_path = "${download_path}/${archive_name}"
 
-  archive { $full_download_path:
-    extract_path => $extract_path,
-    extract      => true,
-    source       => $source,
-    # # determines whether it will download (and subsequently overwrite if extract == 'true')
-    creates      => $creates,
-    cleanup      => true,
-  } ->
+  ensure_package { 'unzip': }
+  exec { "wget ${source} -O ${download_path}": } ->
+  exec { "unzip -u -d ${extract_path}/ ${download_path}": require => Package['unzip'], }
+
+  #  archive { $full_download_path:
+  #    extract_path => $extract_path,
+  #    extract      => true,
+  #    source       => $source,
+  #    # # determines whether it will download (and subsequently overwrite if extract == 'true')
+  #    creates      => $creates,
+  #    cleanup      => true,
+  #  } ->
   # # workaround, as cleanup doesn't seem to work
-  exec { "/bin/rm -f ${full_download_path}": }
-
+  #  exec { "/bin/rm -f ${full_download_path}": }
 }
